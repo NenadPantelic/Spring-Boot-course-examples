8ac9ebb0329c70e7de8115f451db9e4487d67516
NenadPantelic <nenadpantelickg@gmail.com>
2019-10-20 19:47:58 +0200

    Add fifth section - beans

---
+++ Section 5 - Beans/src/main/java/org/spring/course/cars/Engine.java
@@ -0,0 +1,19 @@
+package org.spring.course.cars;
+
+public class Engine {
+
+	private String _type;
+
+	public Engine() {
+		_type = "V8";
+	}
+
+	public String getType() {
+		return _type;
+	}
+
+	public void setType(String type) {
+		_type = type;
+	}
+
+}
